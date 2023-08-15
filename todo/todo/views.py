19df77e1992f8872c6647bef9d33c52b3ab206d6
Divya Patil <92176809+predystopic-dev@users.noreply.github.com>
2023-08-15 16:40:28 +0530

    Fixed views.py importError

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,4 +1,4 @@
-from  django . shortcuts  import  render, redirect
+from  django.shortcuts  import  render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
